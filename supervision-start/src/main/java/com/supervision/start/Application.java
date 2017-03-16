b1927c15f4f11475142a2a469a1bbd0aa8edc997
DESKTOP-PGS8HJU\Zeus Feng <guoyang.fgy@alibaba-inc.com>
2017-03-16 20:11:33 +0800

    增加pom依赖配置

---
+++ supervision-start/src/main/java/com/supervision/start/Application.java
@@ -0,0 +1,29 @@
+package com.supervision.start;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by Zeus Feng on 2017/3/16.
+ *
+ * @author Zeus Feng
+ * @date 2017/03/16
+ */
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+    public static void main(String[] args) throws Exception {
+        List<String> argsList = new ArrayList<String>(Arrays.asList(args));
+        argsList.add("-Xms1024m");
+        argsList.add("-Xmx2048m");
+        //argsList.add("--debug");
+        args = argsList.toArray(new String[argsList.size()]);
+        //args = new String[]{"-Xms1024m","-Xmx2048m"};
+        SpringApplication.run(Application.class,args);
+    }
+}
