5885b5ec2b0ce0055dacc253c5146e11d1305a3e
DESKTOP-PGS8HJU\Zeus Feng <guoyang.fgy@alibaba-inc.com>
2017-03-17 19:09:50 +0800

    渲染主页

---
+++ supervision-web/src/main/java/com/supervision/web/view/Login.java
@@ -0,0 +1,26 @@
+package com.supervision.web.view;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Zeus Feng on 2017/3/17.
+ *
+ * @author Zeus Feng
+ * @date 2017/03/17
+ */
+@RestController
+@RequestMapping(value = "/")
+public class Login {
+
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public ModelAndView loginPage(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+}
